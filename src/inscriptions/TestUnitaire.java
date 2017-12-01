8482ef418c0046fca4abef7a7ee42d6527950427
Eight-sama <exoth.ousha@gmail.com>
2017-12-01 10:32:17 +0100

    Added JUnit.java

--- src/inscriptions/TestUnitaire.java
+++ src/inscriptions/TestUnitaire.java
@@ -1,14 +1,38 @@
 package inscriptions;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import java.time.LocalDate;
+import org.junit.Test;
 
-class TestUnitaire {
 
+
+class TestsJUnit {
+	
+	private LocalDate ld = LocalDate.now();
+	private Inscriptions inscriptions = Inscriptions.getInscriptions();
+	private Competition competition = new Competition(inscriptions,"League Of Legends World Championships",ld,true);
+	private Equipe equipe = new Equipe (inscriptions,"Ginette");
+	private Personne personne = new Personne (inscriptions,"Mesle","Alexandre","lepurificateur@godlike.com");
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testCompetition()
+	{
+		assertEquals("Date de clôture de la compétition : ",LocalDate.now(), competition.getDateCloture());
+		assertEquals("Nom de la compétition : ","League Of Legends World Championships", competition.getNom());		
 	}
-
+	
+	@Test
+	public void testEquipe()
+	{
+		assertEquals("Membres de l'équipe : ", equipe.getMembres(), equipe.getMembres());
+	}
+	
+	@Test
+	public void testPersonne()
+	{
+		assertEquals("Mail : ", "lepurificateur@godlike.com", personne.getMail());
+		assertEquals("Prénom : ", "Alexandre", personne.getPrenom());
+		assertEquals("Nom : ", "Mesle", personne.getNom());
+	}
 }
