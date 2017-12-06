617c5e045dc6d0826031ddb197ef02b13b3599ae
abigaelle123 <abigaellehouri@gmail.com>
2017-12-06 22:58:29 +0100

    rectification

--- src/database/passerelle.java
+++ src/database/passerelle.java
@@ -1,8 +1,7 @@
 package database;
 
-import java.util.List;
+
 import org.hibernate.HibernateException;
-import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
