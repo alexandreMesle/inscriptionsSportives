55396af6656dd5997bf05b1e54f2dbc1ff2f43b2
abigaelle123 <abigaellehouri@gmail.com>
2017-12-07 12:08:45 +0100

    test unitaire getEquipe class personne

--- src/Junit/PersonneTest.java
+++ src/Junit/PersonneTest.java
@@ -1,7 +1,14 @@
 package Junit;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.time.LocalDate;
+
 import org.junit.Test;
+
+import app.Competition;
+import app.Equipe;
 import app.Inscriptions;
 import app.Personne;
 
@@ -22,6 +29,39 @@ public class PersonneTest {
 		String inscription = Personne.getPrenom();
 		assertEquals("d",inscription);
 	}
+	
+	@Test
+	public void testGetMail() {
+		Inscriptions inscription = Inscriptions.getInscriptions();
+		Personne Personne = inscription.createPersonne("a", "b", "c");
+		String m = Personne.getMail();
+		assertEquals("c",m);
+	}
+	@Test
+	
+	public void testSetMail() {
+		Inscriptions inscription = Inscriptions.getInscriptions();
+		Personne Personne = inscription.createPersonne("a", "b", "c");
+		Personne.setMail("d");
+		String m = Personne.getMail();
+		assertEquals("d",m);
+	}
+	
+	@Test
+	public void testGetEquipes() {
+		
+		Inscriptions inscri = Inscriptions.getInscriptions();
+		Personne Personne = inscri.createPersonne("a", "b", "c");
+		Equipe Equipe = inscri.createEquipe("1");
+		Equipe Equipe1 = inscri.createEquipe("2");
+		Equipe.add(Personne);
+		Equipe1.add(Personne);
+		assertTrue(Personne.getEquipes().contains(Equipe));
+		assertTrue(Personne.getEquipes().contains(Equipe1));
+	}
+	
+
+
 	
 	
 }
