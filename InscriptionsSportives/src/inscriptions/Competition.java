4894d3a308ebf5b076ec0c0e7aa5f904cb847a96
Mamadian <Mamadian@DESKTOP-4CECU91>
2019-11-08 23:53:13 +0100

    ajout d'une équipe à une compétition

--- InscriptionsSportives/src/inscriptions/Competition.java
+++ InscriptionsSportives/src/inscriptions/Competition.java
@@ -136,10 +136,13 @@ public class Competition implements Comparable<Competition>, Serializable
 	public boolean add(Equipe equipe)
 	{
 		// TODO vérifier que la date de clôture n'est pas passée
+		if(inscriptionsOuvertes()) {
 		if (!enEquipe)
 			throw new RuntimeException();
 		equipe.add(this);
 		return candidats.add(equipe);
+		}
+		return false;
 	}
 	
 	/**
