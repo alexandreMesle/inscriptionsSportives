3a3d12f0dea9deca2f07ce311545e4ec33f676dc
Mamadian <Mamadian@DESKTOP-4CECU91>
2019-11-22 10:29:47 +0100

    ajout des testUnitaire de la classe Candidat

---
+++ InscriptionsSportives/src/testUnitaire/TestCandidat.java
@@ -0,0 +1,13 @@
+package testUnitaire;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import inscriptions.Inscriptions;
+
+public class TestCandidat {
+
+	//Le test de la classe Candidat s'opère à travers les classes filles tel que Equipe et Personne
+
+}
